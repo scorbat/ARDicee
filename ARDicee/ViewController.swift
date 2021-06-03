ff28aed46e35569ddfa1ac80667e20404f470f1f
Scott Corbat <scorbat@riis.com>
2021-06-03 09:48:22 -0400

    refactored code

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -18,26 +18,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
-        
         // Set the view's delegate
         sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        //let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-//        let sphere = SCNSphere(radius: 0.2)
-//        let material = SCNMaterial()
-//        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
-//
-//        sphere.materials = [material]
-//
-//        let node = SCNNode()
-//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-//        node.geometry = sphere
-//
-//        sceneView.scene.rootNode.addChildNode(node)
         sceneView.autoenablesDefaultLighting = true
     }
     
@@ -100,27 +85,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                 let results = sceneView.session.raycast(query)
                 
                 if let result = results.first {
-                    //add dice to scene
-                    let scene = SCNScene(named: "art.scnassets/diceCollada copy.scn")!
-                    let diceNode = scene.rootNode.childNode(withName: "Dice", recursively: true)
-
-                    diceNode?.position = SCNVector3(
-                        x: result.worldTransform.columns.3.x,
-                        y: result.worldTransform.columns.3.y + diceNode!.boundingSphere.radius,
-                        z: result.worldTransform.columns.3.z
-                    )
-                    
-                    diceArray.append(diceNode!)
-                    
-                    sceneView.scene.rootNode.addChildNode(diceNode!)
-                    
-                    roll(diceNode!)
+                    addDice(at: result)
                 }
             }
         }
     }
     
-    //MARK: my functions
+    //MARK: - my functions
     
     func rollAll() {
         if !diceArray.isEmpty {
@@ -137,5 +108,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         dice.runAction(
             SCNAction.rotateBy(x: CGFloat(randomX), y: 0, z: CGFloat(randomZ), duration: 0.5)
         )
+    }
+    
+    func addDice(at location: ARRaycastResult) {
+        //add dice to scene
+        let scene = SCNScene(named: "art.scnassets/diceCollada copy.scn")!
+        let diceNode = scene.rootNode.childNode(withName: "Dice", recursively: true)
+
+        diceNode?.position = SCNVector3(
+            x: location.worldTransform.columns.3.x,
+            y: location.worldTransform.columns.3.y + diceNode!.boundingSphere.radius,
+            z: location.worldTransform.columns.3.z
+        )
+        
+        diceArray.append(diceNode!)
+        
+        sceneView.scene.rootNode.addChildNode(diceNode!)
+        
+        roll(diceNode!)
     }
 }
